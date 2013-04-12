e091f26bdc3668043aaac7b74e95c02141f27924
Alvar Hansen <alluxxii@gmail.com>
2013-04-12 17:54:52 +0300

    kept only function that now outputs receipt, sent by apple

--- validateaction.php
+++ validateaction.php
@@ -1,15 +1,15 @@
 <?php
 	
-    function validateReceipt($receipt, $isSandbox = true)
-    {
+    function validateReceipt($receipt, $isSandbox = true){
+        if(strpos($receipt,'{') !== false){
+            $receipt = base64_encode($receipt);
+        }
 
         if ($isSandbox) {
             $endpoint = 'https://sandbox.itunes.apple.com/verifyReceipt';
-			print "Environment: Sandbox (use 'sandbox' URL argument to toggle)<br />";
         }
         else {
             $endpoint = 'https://buy.itunes.apple.com/verifyReceipt';
-			print "Environment: Production (use 'sandbox' URL argument to toggle)<br />";
         }
 
         $postData = json_encode(
@@ -18,8 +18,8 @@
        
 
         $ch = curl_init($endpoint);
-		curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
@@ -39,37 +39,12 @@
             throw new Exception('Invalid response data');
         }
  
-        if (!isset($data->status) 
-		|| $data->status != 0) 
-		{
-			print 'Status Code: '. $data->status . '<br/>';
+        if (!isset($data->status) || $data->status != 0) {
+//			print 'Status Code: '. $data->status . '<br/>';
             throw new Exception('Invalid receipt');
         }
 
-        return array(
-            'quantity'       =>  $data->receipt->quantity,
-            'product_id'     =>  $data->receipt->product_id,
-            'transaction_id' =>  $data->receipt->transaction_id,
-            'purchase_date'  =>  $data->receipt->purchase_date,
-            'app_item_id'    =>  $data->receipt->app_item_id,
-            'bid'            =>  $data->receipt->bid,
-            'bvrs'           =>  $data->receipt->bvrs
-        );
+        return $data->receipt;
     }
 
-
-    $receipt   = $_GET['receipt'];
-    $isSandbox = (bool) $_GET['sandbox'];
- 
-    try {
-         if(strpos($receipt,'{') !== false)
-{
-$receipt = base64_encode($receipt);
-}
-        $info = validateReceipt($receipt, $isSandbox);
-		echo 'Success';
-    }
-    catch (Exception $ex) {
-		echo $ex->getMessage().'<br />';
-    }
-?>	
+?>
